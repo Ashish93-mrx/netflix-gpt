c4d517e5ea9c60e0a265171957b4746777213fd1
Ashish <ashishcoorg46@gmail.com>
2025-05-16 21:33:38 +0530

    fixed: mail && password empty check, video description width increased

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,7 @@
 export const checkValidData = (email, password) => {
+    if(email.length==0) return 'Enter the Email'
+    if(password.length==0) return 'Enter the Password'
+
     const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
 
     const isPasswordValid = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/.test(password);
